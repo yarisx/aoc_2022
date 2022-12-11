c0ff4d2e76f43e1fe67e5be37e7bb25a6850e376
Yaroslav Maslennikov <ymaslenn@cisco.com>
2022-12-11 08:58:19 +0100

    Haskell, day 9, part 2

--- day9/day9/src/Lib.hs
+++ day9/day9/src/Lib.hs
@@ -2,16 +2,17 @@ module Lib
     ( someFunc
     ) where
 
-import Data.Char
 import Data.List.Split
 import Data.Set
 
-type TlSet = Set (Int, Int)
+type Point = (Int, Int)
 
 someFunc :: IO ()
 someFunc = do
         contents <- readFile "../input.txt"
-        print . size . fromList . thrd . Prelude.foldl move ((0,0), (0,0), [(0,0)]) . reverse . Prelude.foldl convmove [] . lines $ contents
+        --print . size . fromList . thrd . Prelude.foldl move ((0,0), (0,0), [(0,0)]) . reverse . Prelude.foldl convmove [] . lines $ contents
+        print . size . fromList . thrd . Prelude.foldl move ((0,0), replicate 1 (0,0), [(0,0)]) . reverse . Prelude.foldl convmove [] . lines $ contents
+        print . size . fromList . thrd . Prelude.foldl move ((0,0), replicate 9 (0,0), [(0,0)]) . reverse . Prelude.foldl convmove [] . lines $ contents
 
 -- where to move and how much
 convmove :: [String] -> String -> [String]
@@ -20,20 +21,22 @@ convmove moves ln = (replicate nn f) ++ moves
         (f:n:[]) = splitOn " " ln
         nn = read n :: Int
 
-mov :: String -> (Int, Int) -> (Int, Int)
+mov :: String -> Point -> Point
 mov "R" (hx, hy) = (hx + 1, hy)
 mov "L" (hx, hy) = (hx - 1, hy)
 mov "U" (hx, hy) = (hx, hy + 1)
 --mov "D" (hx, hy) = (hx, hy - 1)
 mov _ (hx, hy) = (hx, hy - 1)
 
-move (hd, tl, tlpoints) f = (newhd, newtl, newtlpoints)
+move :: (Point, [Point], [Point]) -> String -> (Point, [Point], [Point])
+move (hd, tails, tlpoints) f = (newhd, tail . reverse $ newtails', newtlpoints)
     where
         newhd = mov f hd
-        newtl = tailMove newhd tl
+        newtails' = Prelude.foldl longtailMove [newhd] tails
+        newtl = head newtails'
         newtlpoints = (newtl:tlpoints)
 
-tailMove :: (Int, Int) -> (Int, Int) -> (Int, Int)
+tailMove :: Point -> Point -> Point
 tailMove (hx, hy) (tx, ty)
     | sgx * (hx - tx) > 1 = (tx + sgx, ty + sgy)
     | sgy * (hy - ty) > 1 = (tx + sgx, ty + sgy)
@@ -42,4 +45,8 @@ tailMove (hx, hy) (tx, ty)
         sgx = signum (hx - tx)
         sgy = signum (hy - ty)
 
+longtailMove :: [Point] -> Point -> [Point]
+longtailMove tails x = (tailMove (head tails) x:tails)
+
+thrd :: (x, y, a) -> a
 thrd (_, _, f) = f
