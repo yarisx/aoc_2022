09f685cac722de6800f68b5a751dfac12c182ac4
Yaroslav Maslennikov <ymaslenn@cisco.com>
2022-12-15 12:08:09 +0100

    Haskell, day 14, part 2

--- day14/day14/src/Lib.hs
+++ day14/day14/src/Lib.hs
@@ -17,7 +17,7 @@ type Slice = (Int, [Int])
 someFunc :: IO ()
 someFunc = do
     contents <- readFile "../input.txt"
-    print . sandgrain . expand_lines 0 [] . expand_lines' . Prelude.foldl parse [] . lines $ contents
+    print . sandgrain . expand_lines 1 [] . expand_lines' . Prelude.foldl parse [] . lines $ contents
 
 -- does a set of linespecs from a string ("1,2 -> 3,4 -> 5,6" -> [((1,2),(3,4)), ((3,4),(5,6))]
 parse :: [Line] -> String -> [Line]
@@ -52,34 +52,37 @@ expand_line expanded ((sx, sy), (ex, ey))
 expand_lines' :: [Line] -> [Slice]
 expand_lines' = Prelude.map (\(x, l) -> (x, sortUniq l)) . Data.MultiMap.assocs . Prelude.foldl expand_line empty --linespecs
 
+fill_line c off = take ((off + 1) * 2 + 1) [(c - off - 1)..]
+
 expand_lines :: Int -> [Slice] -> [Slice] -> [Slice]
-expand_lines _ res [] = reverse res
+expand_lines _ res [] = reverse ((vspan, fill_line 500 vspan): (max_y + 1, []) : res)
+    where
+        max_y = fst . head $ res
+        vspan = max_y + 2
 expand_lines mmin res prefill
     | mmin < prefill_y = expand_lines (mmin + 1) ((mmin, []):res) prefill
     | mmin == prefill_y = expand_lines (mmin + 1) (head prefill : res) . tail $ prefill
     where
         prefill_y = fst . head $ prefill
 
-show :: PointMap -> [(Int, [Int])]
-show m = Data.MultiMap.assocs m
-
 sandgrain :: [Slice] -> Int
-sandgrain pm = sandgrain' pm (500,0) 0 []
+sandgrain pm = sandgrain' pm 500 0 []
 
 -- j is the _next_ slice, vpm are those that are checked, so the grain is -between- slices
--- if the grain is stuck then topmost/last from the checked is updated with the grains position
+-- if the grain is stuck then topmost/last from the checked is updated with the grain's position
 
-sandgrain' :: [Slice] -> Point -> Int -> [Slice] -> Int
-sandgrain' [] _ cnt _vpm = cnt
-sandgrain' s@(j:slices) (gx, gy) cnt vpm
-    | nextx /= Nothing = sandgrain' slices (fromMaybe (-1) nextx, gy + 1) cnt (j:vpm)
-    | otherwise = sandgrain' newslices (500, 0) (cnt + 1) []
+sandgrain' :: [Slice] -> Int -> Int -> [Slice] -> Int
+sandgrain' [] _ cnt _ = cnt
+sandgrain' s@(j:slices) gx cnt vpm
+    | nextx /= Nothing = sandgrain' slices (fromMaybe (-1) nextx) cnt (j:vpm)
+    | vpm == [] = cnt + 1
+    | otherwise = sandgrain' newslices 500 (cnt + 1) []
     where
         nextx = find (\x -> not (elem x jxs)) [gx, (gx - 1), (gx + 1)]
-        newslices = reverse (blocked: tail vpm) ++ s
+        newslices = Prelude.map (\(x, l) -> (x, sortUniq l)) (reverse (blocked: (tail vpm))) ++ s
         blocked = (iy, (gx:ixs))
         (iy, ixs) = head vpm
-        (_, jxs) = j
+        (jy, jxs) = j
 
 fromMaybe :: a -> Maybe a -> a
 fromMaybe def Nothing = def
